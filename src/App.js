7baafd94dab7692cf726b09526ae3559939a9d0f
Roman375 <romanlviv96@gmail.com>
2020-10-31 15:04:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react'
+import { Input } from 'antd';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Input.Search
+      placeholder="input search text"
+      allowClear
+      style={{ width: 200, margin: '0 10px' }}
+    />
     </div>
   );
+}
 }
 
 export default App;
