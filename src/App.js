1fa76ec7163dfb79905f1c92dcfc9ba6621554aa
Roman375 <romanlviv96@gmail.com>
2020-11-02 21:16:40 +0200

    das

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="wrapper">
-        {/* <Auth /> */}
-        <Route exact path='/ms' component={HomeContainer} />
-        {/* <div>{fetch("https://api.chucknorris.io/jokes/random").then(r =>  r.json().then(data => ({status: r.status, data: data}))).then(data => console.log(data.data.value))}</div> */}
+        <Route exact path='/' component={HomeContainer} />
       </div>
     )
   }
